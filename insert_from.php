60451fdba344887b19fc6b03cc3a10a62d9bbb06
mindmintg <76504728+mindmintg@users.noreply.github.com>
2021-02-10 10:37:23 +0700

    1.4

---
+++ insert_from.php
@@ -0,0 +1,21 @@
+<h3>เพิ่มภาพยนต์<small><a href=".">ย้อนกลับ</a></small></h3>
+
+<form action="insert.php" method="post">
+    <label for="major_id">รหัสภาพยนต์ :</label>
+    <input type="text" name="major_id" id="major_id">
+    <br><br>
+    <label for="major_name">ชื่อภาพยนต์ :</label>
+    <input type="text" name="major_name" id="major_name">
+    <br><br>
+    <label for="major_day">วันฉาย :</label>
+    <input type="datetime" name="major_day" id="major_day">
+    <br><br>
+    <label for="major_member">ชื่อผู้ใช้งาน :</label>
+    <input type="text" name="major_member" id="major_member">
+    <br><br>
+    <label for="major_pin">pin :</label>
+    <input type="text" name="major_pin" id="major_pin">
+    <br><br>
+    <button type="submit">ยืนยัน</button>
+    <button type="reset">ล้างข้อมูล</button>
+</form>
